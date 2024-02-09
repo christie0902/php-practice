a16612fa55a5c9878e835dca7ea2138ef97b3cc9
Nhi <nhipham0902@gmail.com>
2024-02-09 16:37:46 +0100

    complete logic to sort the movies based on GET request

---
+++ sortMovies/movie.php
@@ -0,0 +1,38 @@
+<?php
+require 'data.php';
+require_once 'function.php';
+
+$sort_by = $_GET['sortby'] ?? null;
+$sort_way = $_GET['sortway'] ?? null;
+if ($sort_by !== null && $sort_way !== null) {
+    $movies = sortMovies($movies, $sort_by, $sort_way);
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Movie Sort</title>
+</head>
+<body>
+    <a href="?sortby=title&sortway=asc" style='color:purple'>Sort by title A-Z</a><br>
+    <a href="?sortby=title&sortway=desc" style='color:purple'>Sort by title Z-A</a><br>
+    <a href="?sortby=rating&sortway=desc" style='color:green'>Sort by rating from the higest</a><br>
+    <a href="?sortby=rating&sortway=asc" style='color:green'>Sort by rating from the lowest</a><br>
+    <a href="?sortby=year&sortway=desc" style='color:red'>Sort by year from the newest</a><br>
+    <a href="?sortby=year&sortway=asc" style='color:red'>Sort by year from the oldest</a><br>
+
+    
+
+    <?php foreach($movies as $movie) : ?>
+        <div>
+            <li>
+                <?="Title: {$movie['title']}</br>Rating: {$movie['rating']}</br>Year: {$movie['year']}</br>"?> 
+            </li>
+        </div>
+    <?php endforeach ?>
+
+
+</body>
+</html>
